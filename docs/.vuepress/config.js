fd2d65113caf668ee562a86d515db46c9039ca07
wyy <807515004@qq.com>
2022-04-22 22:36:10 +0800

    feat: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  // 站点配置
+  lang: 'zh-CN',
+  title: '马里奥',
+  description: '这是我的第一个 VuePress 站点',
+  head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: '/images/logo.png',
+    // repo: 'vuejs/vuepress',
+    navbar: [
+      { text: '主页', link: '/' },
+      // { text: '关于', link: '/about/' },
+      { text: 'GitHub', link: 'https://github.com/CoderYuanHub' },
+    ],
+    sidebar: [
+      {
+        text: '开始吧',
+        link: '/',
+      },
+      {
+        text: '博主正在努力中～',
+        link: '/guide/',
+      }
+    ],
+    notFound: [
+      "没有找到你想要的页面",
+      "你可以尝试搜索",
+      "或者返回首页",
+      "或者返回上一页",
+      "你调皮了哦",
+    ],
+    backToHome: "我要回首页了～",
+  },
+}
