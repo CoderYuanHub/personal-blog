050841db40071e39a7836b44eabdcf520ea69b07
wyy <807515004@qq.com>
2022-05-01 17:53:22 +0800

    feat(git): 新增自动化流程

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ module.exports = {
   title: '马里奥',
   description: '这是我的第一个 VuePress 站点',
   head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+  base: '/',
 
   // 主题和它的配置
   theme: '@vuepress/theme-default',
