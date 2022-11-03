4d2edabd99f6d8d35c10a2fa6632c8f1b323650b
王阳源 <807515004@qq.com>
2022-11-03 09:45:50 +0800

    fix: 自定义主题出错

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,48 +1,49 @@
-const { path } = require('@vuepress/utils');
-module.exports = {
-  host: 'localhost',
-  port: '8080',
-  // 站点配置
-  lang: 'zh-CN',
-  title: '马里奥',
-  description: '这是我的第一个 VuePress 站点',
-  head: [['link', { rel: 'icon', href: '/personal-blog/images/logo.png' }]],
-  base: '/personal-blog/',
+import { defineUserConfig } from "vuepress";
+import { defaultTheme } from "@vuepress/theme-default";
 
-  // 主题和它的配置
-  // theme: '@vuepress/theme-default',
-  theme: path.resolve(__dirname, './theme'),
-  themeConfig: {
-    logo: '/images/logo.png',
-    // repo: 'vuejs/vuepress',
+export default defineUserConfig({
+  lang: "zh-CN",
+  title: "你好， VuePress ！",
+  description: "这是我的第一个 VuePress 站点",
+  // name: "vuepress-theme-foo-test",
+  host: "localhost",
+  port: "8080",
+  // 站点配置
+  lang: "zh-CN",
+  title: "马里奥",
+  description: "这是我的第一个 VuePress 站点",
+  head: [["link", { rel: "icon", href: "/images/logo.png" }]],
+  base: "/",
+  theme: defaultTheme({
+    logo: "/images/logo.png",
     navbar: [
-      { text: '主页', link: '/' },
+      { text: "主页", link: "/" },
       // { text: '关于', link: '/about/' },
-      { text: 'GitHub', link: 'https://github.com/CoderYuanHub' },
+      { text: "GitHub", link: "https://github.com/CoderYuanHub" },
     ],
     sidebar: [
       {
-        text: '开始吧',
-        link: '/',
+        text: "开始吧",
+        link: "/",
       },
       {
-        text: 'Vue新篇章',
+        text: "Vue新篇章",
         collapsible: true,
-        children: ['/vue/vmodel.md']
+        children: ["/vue/vmodel.md"],
       },
       {
-        text: '工具方法',
+        text: "工具方法",
         collapsible: true,
-        children: ['/tools/export-to-world.md','/tools/the-patch.md']
+        children: ["/tools/export-to-world.md", "/tools/the-patch.md"],
       },
       // {
       //   text: "自定义组件界面",
       //   link: "/main/main.html",
       // },
       {
-        text: '博主正在努力中～',
-        link: '/guide/',
-      }
+        text: "博主正在努力中～",
+        link: "/guide/",
+      },
     ],
     notFound: [
       "没有找到你想要的页面",
@@ -52,13 +53,6 @@ module.exports = {
       "你调皮了哦",
     ],
     backToHome: "我要回首页了～",
-  },
-  plugins: [
-    [
-      '@vuepress/plugin-register-components',
-      {
-        componentsDir: path.resolve(__dirname, './components')
-      }
-    ]
-  ]
-}
+  }),
+  // repo: 'vuejs/vuepress',
+});
